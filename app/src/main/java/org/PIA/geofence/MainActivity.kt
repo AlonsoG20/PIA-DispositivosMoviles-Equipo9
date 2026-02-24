48ac8694c5e4df27029caef76cafd0ad65e01e5b
Angel Sosa <angelsogu23@gmail.com>
2026-02-24 14:04:23 -0600

    se dirige el fragment cuenta al iniciar sesion

--- app/src/main/java/org/PIA/geofence/MainActivity.kt
+++ app/src/main/java/org/PIA/geofence/MainActivity.kt
@@ -1,20 +1,43 @@
 package org.PIA.geofence
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import org.PIA.geofence.ui.cuenta.CuentaFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        if (savedInstanceState == null) {
+            loadFragment(CuentaFragment())
+        }
+
+        val navCuenta = findViewById<LinearLayout>(R.id.nav_cuenta)
+        val navControl = findViewById<LinearLayout>(R.id.nav_control)
+        val navRegistros = findViewById<LinearLayout>(R.id.nav_registros)
+
+        navCuenta.setOnClickListener {
+            loadFragment(CuentaFragment())
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/org/PIA/geofence/ui/cuenta/CuentaFragment.kt
@@ -0,0 +1,13 @@
+package org.PIA.geofence.ui.cuenta
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import org.PIA.geofence.R
+
+class CuentaFragment : Fragment(R.layout.fragment_cuenta) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
